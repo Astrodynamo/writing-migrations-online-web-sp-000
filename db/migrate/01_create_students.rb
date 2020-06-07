c83de3812dad803b0a0d0c4784826fc9b3584fe8
Amanda King <amapickell@gmail.com>
2020-06-07 00:25:48 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  
   def change
-    create_table do |t|(
-      t.name => string
-    )
+    create_table :students do |t|
+      t.string :name
+    end
   end
+  
 end
