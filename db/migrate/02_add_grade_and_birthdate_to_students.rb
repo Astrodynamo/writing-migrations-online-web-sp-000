87ffe98c8bb9cf72462ed0fef4f848d36b9e692a
Amanda King <amapickell@gmail.com>
2020-06-07 00:46:07 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < AcitveRecord::Migration
   
   def change
-    add_column :students, :birthdate, :string,
+    add_column :students, :birthdate, :string
     add_column :students, :grade, :integer
   end
   
